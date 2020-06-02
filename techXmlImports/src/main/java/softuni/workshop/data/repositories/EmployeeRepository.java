d394beabe8749620009fcc997c4256c491995b02
GinkaKuzmanov <ginkakuzmanova@gmail.com>
2020-06-02 23:22:11 +0300

    Simple MVC project using Spring boot.

--- techXmlImports/src/main/java/softuni/workshop/data/repositories/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package softuni.workshop.data.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import softuni.workshop.data.entities.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-
-}
